ee61dc3c1ed47ac7ee8b7a318b352069af84fd13
다다미 <82502353+dadami-io@users.noreply.github.com>
2021-08-18 14:25:08 +0900

    .
    최적화좀해주세요.,ㅠ

--- main.py
+++ main.py
@@ -1,91 +1,108 @@
-# 한글 분석을 위한 코엔엘파이(konlpy) 설치
-# !pip install konlpy 
-
-# 코랩에서 한글 표기를 위해 나눔 글꼴 설치
-# !apt-get update -qq
-# !apt-get install fonts-nanum* -qq  
-
-
-import requests                     # HTML 요청 처리
-from bs4 import BeautifulSoup       # 웹 크롤링을 위한 패키지 BeautifulSoup 설치
+import requests
+from bs4 import BeautifulSoup
 import matplotlib.pyplot as plt
-import nltk                         # Natural Language Toolkit 기호 및 통계 자연어 처리를 위한 라이브러리
-from konlpy.tag import Kkma         # 오픈소스 한국어 분석기
-from konlpy.tag import Okt          # 오픈소스 한국어 분석기
-from wordcloud import WordCloud     
+import nltk
+from konlpy.tag import Okt
+from konlpy.tag import Kkma
+from wordcloud import WordCloud
+from io import BytesIO
+import time
 
-# 뉴스 기사를 크롤링하기 위한 날짜를 변수에 저장
+tw = Okt()
 query = '코로나'
-
-# 클롤링하기 위한 링크를 변수에 저장
-news_url = f'https://search.naver.com/search.naver?query={query}&where=news&ie=utf8&sm=nws_hty'
-
-# 안티 크롤링(Python 코드가 아닌 Mozilla 브라우저로 접속하는 것처럼 우회)
-# 일반적으로 각 웹사이트는 해킹, 불법 접속 등을 막기 위해 프로그래머가 코드를 통해 접근하는 것을 방지해놓음
-# 브라우저로 접속하는 것처럼 User-Agent에 Mozilla, Chrome 등을 작성하여 우회하는 것을 안티 크롤링이라고 함
+news_url = 'https://search.naver.com/search.naver?query={}&where=news&ie=utf8&sm=nws_hty'
 headers = {'User-Agent':'Mozilla/5.0'}
 
-# BeautifulSoupdml의 requests.get을 이용하여 HTML을 text로 수집하여 변수에 저장
-req = requests.get(news_url, headers = headers)
+def getImg(query=query):
+    first=time.time()
+    print(query)
+    req = requests.get(news_url.format(query), headers = headers)
+    soup = BeautifulSoup(req.text, 'html.parser')
+    news_titles = soup.select('div.news_wrap.api_ani_send > div > a')
+    crowled_title = []
+    for i in range(len(news_titles)):
+        crowled_title.append(news_titles[i].text)
+        print(i+1, news_titles[i].text)
 
-# HTML 페이지에서 뉴스 기사 제목이 저장된 위치 저장
-soup = BeautifulSoup(req.text, 'html.parser')
-# print(soup)
-news_titles =  soup.select('div.news_wrap.api_ani_send > div > a')
+    title = "".join(crowled_title)
+    filter_text = ['.','"',',',"'",'·','=','\n','[',']','“','”','‘','’','?','!','…','vs', '▶','→','(',')','+']
+    for text in filter_text:
+        title = title.replace(text, ' ')
+    filtered_title = title
 
-# 기사 제목 리스트 저장하기
-crowled_title = []
-for i in range(len(news_titles)):
-    crowled_title.append(news_titles[i].text)
-    print(i+1, news_titles[i].text) 
-    
-# 뉴스 제목에서 특수 기호 제거하기
-title = "".join(crowled_title)
-filter_text = ['.','"',',',"'",'·','=','\n','[',']','“','”','‘','’','?','!','…','vs', '▶','→','(',')','+']
-for text in filter_text:
-    title = title.replace(text, ' ')
-filtered_title = title
+    second=time.time()
 
-# Konlpy를 이용하여 기사 제목 내 명사만 추출하기
-tw = Okt() 
-tokens_ko = tw.nouns(filtered_title)
-tokens_ko
-ko = nltk.Text(tokens_ko, name='기사 내 명사')
-ko.tokens
-ko.vocab()
+    tokens_ko = tw.nouns(filtered_title)
+    tokens_ko
+    ko = nltk.Text(tokens_ko, name='기사 내 명사')
+    ko.tokens
+    ko.vocab()
 
-# 단어 길이가 1 이상이면서 '단독' 또는 ' '(공백)이 아닌 단어를 new_ko에 추가하기
-new_ko=[]
-for word in ko:
-  if len(word) > 1 and word != '단독' and  word != ' ':
-        new_ko.append(word)
-print(new_ko)
+    third=time.time()
 
-# 단어 빈도를 기준으로 내림차순 정렬 후 상위 150개 단어 추출
-ko = nltk.Text(new_ko, name = '기사 내 명사 두 번째')
-ko.tokens
-ko.vocab()
-data = ko.vocab().most_common(150)
-print(data)
+    new_ko=[]
+    for word in ko:
+      if len(word) > 1 and word != '단독' and  word != ' ':
+            new_ko.append(word)
+    print(new_ko)
 
-# 단어와 빈도를 dict로 저장하기
-data = dict(data)
-print(data)
+    fourth=time.time()
 
-# 워드 클라우드 출력
-wordcloud = WordCloud().generate(filtered_title)
+    ko = nltk.Text(new_ko, name = '기사 내 명사 두 번째')
+    ko.tokens
+    ko.vocab()
+    data = ko.vocab().most_common(150)
+    print(data)
 
-font = '/usr/share/fonts/truetype/nanum/NanumGothicEco.ttf'
+    data = dict(data)
+    print(data)
 
-wc = WordCloud(font_path=font,\
-		background_color="white", \
-		width=1280, \
-		height=960, \
-		max_words=100, \
-		max_font_size=300)
-wc = wc.generate_from_frequencies(data)
+    fifth=time.time()
+    wordcloud = WordCloud().generate(filtered_title)
+    font = 'NanumGothicEcoR.ttf'
+    wc = WordCloud(font_path=font,\
+            background_color="white", \
+            width=1280, \
+            height=960, \
+            max_words=100, \
+            max_font_size=300)
+    wc = wc.generate_from_frequencies(data)
 
-plt.figure(figsize=(10,10))
-plt.imshow(wc, interpolation='bilinear')
-plt.axis('off')
-plt.show()
+    sixth=time.time()
+
+    plt.figure(figsize=(10,10))
+    plt.imshow(wc, interpolation='bilinear')
+    plt.axis('off')
+
+
+
+    seventh=time.time()
+
+    img = BytesIO()
+    plt.savefig(img, format='png', dpi=200)
+    img.seek(0)
+
+    eighth=time.time()
+
+    print(eighth-first)
+    print(first,second,third,fifth,sixth,seventh,eighth)
+    return img
+
+def getMusic(query):
+        response = "".join([i for i in requests.get("https://api.music.msub.kr/?song="+query).json()["song"][int(0)]["lyrics"].replace("<br>", "\n").split("\n") if i!="" and i!=" "])
+        for text in ['.','"',',',"'",'·','=','\n','[',']','“','”','‘','’','?','!','…','vs', '▶','→','(',')','+']:response = response.replace(text, ' ')
+
+        plt.figure(figsize=(10,10))
+        plt.imshow(WordCloud(font_path='NanumGothicEcoR.ttf',\
+                background_color="white", \
+                width=1280, \
+                height=960, \
+                max_words=100, \
+                max_font_size=300).generate_from_frequencies(dict(nltk.Text([i for i in nltk.Text(Kkma().nouns(response), name='기사 내 명사') if len(i)>1 and i!=" "], name = '기사 내 명사 두 번째').vocab().most_common(150))), interpolation='bilinear')
+        plt.axis('off')
+
+
+        img = BytesIO()
+        plt.savefig(img, format='png', dpi=200)
+        img.seek(0)
+        return img
